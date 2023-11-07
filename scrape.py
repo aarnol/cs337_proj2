c90d3a79118a9ae4bc0576ec18b4a7bbbdbc493c
aarnold1620 <alexarnold2024@u.northwestern.edu>
2023-11-07 15:03:05 -0600

    Cleaned up scraper program

--- scrape.py
+++ scrape.py
@@ -5,17 +5,17 @@ import re
 
 
 def get_recipe_info(url):
-    # Send an HTTP request to the URL and retrieve the content
+    
     response = requests.get(url)
 
     if response.status_code != 200:
         print("Failed to retrieve the webpage.")
         return
 
-    # Parse the webpage content with BeautifulSoup
+ 
     soup = BeautifulSoup(response.text, 'html.parser')
 
-    # Find and extract the recipe title
+ 
     recipe_title = soup.find('span', class_='o-AssetTitle__a-HeadlineText').get_text()
 
     # Find and extract the list of ingredients
@@ -36,8 +36,8 @@ def get_recipe_info(url):
 
     for step in instruction_list:
         step = step.next
-      
-        instructions.append(step)
+        pattern = "^([^A-Za-z]*)"
+        instructions.append(re.sub(pattern, "", step))
 
     return {
         'title': recipe_title,
@@ -46,7 +46,7 @@ def get_recipe_info(url):
     }
 
 if __name__ == '__main__':
-    # Replace the URL with the Food Network recipe URL you want to scrape
+    
     # url = 'https://www.foodnetwork.com/recipes/banana-bread-recipe-1969572'
     url = "https://www.foodnetwork.com/recipes/food-network-kitchen/yogurt-marinated-grilled-chicken-shawarma-9961050"
 
